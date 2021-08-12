7de638ff7e8aa0166a9d52b065f69c11215c1e2d
mongus <kjyanus@gmail.com>
2021-08-12 22:52:05 +0900

    env variable set

--- festival/settings.py
+++ festival/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-dlu)1ah=vpi4knjsm_a$k626^@wjhaq!n$&e!(#q5_$$dn+@0m"
-
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "django-insecure-dlu)1ah=vpi4knjsm_a$k626^@wjhaq!n$&e!(#q5_$$dn+@0m"
+)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") != "False"
 
 ALLOWED_HOSTS = []
 
