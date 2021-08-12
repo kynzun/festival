4f8f99722e52ed4e6d1299ea2f8ff8ede46870b3
mongus <kjyanus@gmail.com>
2021-08-12 17:59:30 +0900

    login redirect mypage

--- account/views.py
+++ account/views.py
@@ -16,12 +16,11 @@ def login_view(request):
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
-            user = authenticate(
-                request=request, username=username, password=password)
+            user = authenticate(request=request, username=username, password=password)
             if user is not None:
                 login(request, user)
 
-        return redirect("home")
+        return redirect("mypage")
     else:
         form = AuthenticationForm()
         return render(request, "login.html", {"form": form})
